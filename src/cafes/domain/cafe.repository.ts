65c635d416e920d8fd1caf67ad8b3cea37999feb
albertoval99 <albertovalabad@gmail.com>
2025-02-12 18:41:13 +0100

    EMPEZANDO FILTRO

--- src/cafes/domain/cafe.repository.ts
+++ src/cafes/domain/cafe.repository.ts
@@ -2,5 +2,5 @@ import Cafe from "./Cafe";
 
 export default interface CafeRepository {
     crearCafe(cafe:Cafe):Promise<Cafe>;
-    // filter(nombre:string,tueste:string,precio:number,origen:string,peso:number,nombreTienda:string,pagina:number):Promise<Cafe[]>;
+    filter(nombre:string,tueste:string,precio:number,origen:string,peso:number,nombreTienda:string,pagina:number):Promise<Cafe[]>;
 }
