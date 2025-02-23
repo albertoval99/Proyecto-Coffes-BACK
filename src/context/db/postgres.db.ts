30ffd3ba61c248fb9e366a3f253b2cb2a7ffcf64
albertoval99 <albertovalabad@gmail.com>
2025-02-24 00:48:58 +0100

    s

--- src/context/db/postgres.db.ts
+++ src/context/db/postgres.db.ts
@@ -1,24 +1,17 @@
 import { Pool } from "pg"; //npm i pg
 import dotenv from "dotenv"; //npm i dotenv
-import fs from "fs"; 
 dotenv.config();
 
 const dbHost = process.env.POSTGRES_HOST;
 const dbUser = process.env.POSTGRES_USER;
 const dbPassword = process.env.POSTGRES_PASSWORD;
 const dbName = process.env.POSTGRES_DB;
-const sslEnabled = process.env.POSTGRES_SSL === 'true';
+
 
 export const pool = new Pool({
   max: 1000,
   connectionString: `postgres://${dbUser}:${dbPassword}@${dbHost}:5432/${dbName}`,
   idleTimeoutMillis: 30000,
-  ssl: sslEnabled ? {
-    rejectUnauthorized: false,  // Este valor se puede poner en true si se quiere una validación más estricta
-    ca: fs.readFileSync('/path/to/ca-cert.pem').toString(),  // Si usas un certificado raíz
-    key: fs.readFileSync('/path/to/client-key.pem').toString(),  // Si tienes una clave cliente
-    cert: fs.readFileSync('/path/to/client-cert.pem').toString() // Si tienes un certificado cliente
-  } : false,
 });
 
 
